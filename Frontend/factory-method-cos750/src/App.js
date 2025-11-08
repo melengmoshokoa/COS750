72a5b14165784b3c7ed04355a97310cc5394c6d4
centia404 <118193986+InnocentiaLedimo@users.noreply.github.com>
2025-11-08 21:10:01 +0200

    Added Dashboard component and supabase client; updated routing;

--- Frontend/factory-method-cos750/src/App.js
+++ Frontend/factory-method-cos750/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Quizzes from './Pages/Quizzes';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
      <Router>
       <Routes>
-       
-        <Route path="/" element={<Quizzes />} />
+
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/quizzes" element={<Quizzes />} />
   
       </Routes>
     </Router>
---
+++ Frontend/factory-method-cos750/src/Pages/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { supabase } from '../supabaseClient';
+
+const Dashboard = () => {
+    console.log(supabase)
+
+
+function Dashboard() {
+  return (
+    <div>
+        <h1>Welcome to the Dashboard Page</h1>
+    </div>
+     
+  );
+}
+}
+
+export default Dashboard;
