1236b5209570d7e9a0a246b7b7ea6fc27bd402c5
Nokubonga Cele <162386423+NokubongaCele@users.noreply.github.com>
2025-11-14 16:18:21 +0200

    Add SuggestedReads route to App component

--- Frontend/factory-method-cos750/src/App.js
+++ Frontend/factory-method-cos750/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Quizzes from './Pages/Quizzes';
 import Dashboard from './Pages/Dashboard';
+import SuggestedReads from './Pages/SuggestedReads';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
         <Route path="/" element={<Dashboard />} />
         <Route path="/Quizzes" element={<Quizzes />} />
+        ‹Route path="/SuggestedReads" element={<SuggestedReads • />}•/>
   
       </Routes>
     </Router>
