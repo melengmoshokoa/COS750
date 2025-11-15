f5892699488576a7ebed6b42521a2b8f6e54d9de
Lesedi Sambo <sambolesedi20017@gmail.com>
2025-11-15 17:11:36 +0200

    Flashcards page completed with background fix

--- Frontend/factory-method-cos750/src/App.js
+++ Frontend/factory-method-cos750/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Quizzes from './Pages/Quizzes';
-import Dashboard from './Pages/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Quizzes from "./Pages/Quizzes";
+import Dashboard from "./Pages/Dashboard";
+import FlashCards from "./Pages/FlashCards";
 
 function App() {
   return (
-     <Router>
+    <Router>
       <Routes>
-
         <Route path="/" element={<Dashboard />} />
         <Route path="/Quizzes" element={<Quizzes />} />
-  
+        <Route path="/Flashcards" element={<FlashCards />} />
       </Routes>
     </Router>
   );
 }
-l
 export default App;
