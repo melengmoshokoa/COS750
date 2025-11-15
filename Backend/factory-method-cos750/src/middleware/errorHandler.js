a4ed28ca5366a52b5f9d56d9be08684c936b8f68
Wadalisa <wadalisamolokwe@gmail.com>
2025-11-15 12:27:23 +0200

    removed unncessaries

--- Backend/factory-method-cos750/src/middleware/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: err.message || 'Server Error' });
-};
--- Backend/factory-method-cos750/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getUser } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/:userId', getUser);
-
-export default router;
