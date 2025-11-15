290c0faa14de73a751cabcf8866d6af3f0e112c9
melengmoshokoa <133795063+melengmoshokoa@users.noreply.github.com>
2025-11-15 22:56:13 +0200

    Added nav to progress page

--- Frontend/factory-method-cos750/src/Pages/Header.js
+++ Frontend/factory-method-cos750/src/Pages/Header.js
@@ -15,7 +15,7 @@ function Header() {
           <Link to="/" className="nav-item">Home</Link>
           <Link to="/QuizDashboard" className="nav-item">Quizzes</Link>
           <Link to="/flashcards" className="nav-item">Flashcards</Link>
-          <Link to="/progress" className="nav-item">Progress</Link>
+          <Link to="/ProgressPage" className="nav-item">Progress</Link>
           <Link to="/StoryBoard" className="nav-item">StoryBoard</Link>
         </nav>
       </div>
